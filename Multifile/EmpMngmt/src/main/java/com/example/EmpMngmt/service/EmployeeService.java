87187c721741932361d27c80d2f202cf51309322
geo_joseph <21mp1838@rit.ac.in>
2023-07-25 22:10:36 +0530

    Index file and folders created

---
+++ Multifile/EmpMngmt/src/main/java/com/example/EmpMngmt/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.EmpMngmt.service;
+
+import com.example.EmpMngmt.dto.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public interface EmployeeService {
+
+    public Employee addEmployee(Employee employee);
+
+    public Optional<Employee> getEmployee(int empId);
+}
---
+++ Multifile/EmpMngmt/src/main/java/com/example/EmpMngmt/service/EmployeeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.EmpMngmt.service;
+
+import com.example.EmpMngmt.dao.EmployeeDao;
+import com.example.EmpMngmt.dto.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        return employeeDao.save(employee);
+    }
+
+    @Override
+    public Optional<Employee> getEmployee(int empId) {
+        return employeeDao.findById(empId);
+    }
+}
