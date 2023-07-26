f7145a5dec60df69b122512ca60226e0e75c9768
geo_joseph <21mp1838@rit.ac.in>
2023-07-26 19:07:57 +0530

    JS update: Generate button now generates 5 variant colors of a random hue

--- Color Palette Maker/scripts/palettegen.js
+++ Color Palette Maker/scripts/palettegen.js
@@ -1,12 +1,34 @@
-const hue = 120; // 0-360
-const saturation = 100; // 0-100
-const lightness = 50; // 0-100
+// Color palette generator
 
-const hslColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+const numOfPalettes = 5;
+const shuffle = 0;
+
+var h;
+var s;
+var l;
+var hslColor;
+
+// const hue = 120; // 0-360
+// const saturation = 100; // 0-100
+// const lightness = 50; // 0-100
+
+// Random number generation
+// ran = Math.floor(Math.random() * (max - min) + min);
+// ran = Math.floor(Math.random() * max);
+
+// hslColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+
 
 function palettegen()
 {
-    for (let i = 1; i<10; i++) {
+    h = Math.floor(Math.random() * 361);
+    for (let i = 1; i<6; i++) {
+        
+        s = Math.floor(Math.random() * 101);
+        l = Math.floor(Math.random() * 101);
+
+        hslColor = `hsl(${h}, ${s}%, ${l}%)`;
+
         document.getElementById(`b${i}`).style.backgroundColor = hslColor;
     }
 }
